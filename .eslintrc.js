c8c63aa2fa5b056720ee1913ac323e811a176dd6
Marco Rodrigues <marcoaurelio853@gmail.com>
2022-04-25 00:00:59 +0100

    vuex upgrade

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,14 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/vue3-recommended", "eslint:recommended", "prettier"],
   parserOptions: {
     parser: "babel-eslint",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    //  "vetur.validation.template": false,
+    "vue/require-default-prop": "off",
   },
 };
--- src/components/task/TodoListItem.vue
+++ src/components/task/TodoListItem.vue
@@ -1,38 +1,48 @@
 <template>
   <div
-    class="bg-white shadow-sm rounded-md text-gray-700 text-xs md:text-sm p-4"
+    class="bg-white shadow-sm rounded-md text-gray-700 flex flex-col"
     :class="{ 'opacity-25 line-through': task.done }"
   >
-    <div>{{ task.description }}</div>
-    <div class="py-4 bg-white">
+    <div class="p-4 border-b border-gray-100">{{ task.description }}</div>
+    <div class="p-4 bg-white flex-grow">
       <BaseCheckbox
         class="mb-2"
         @update:model-value="$emit('update:done', $event)"
         :model-value="done"
-        >Done</BaseCheckbox
-      >
+      >Done</BaseCheckbox>
       <BaseCheckbox
         @update:model-value="$emit('update:priority', $event)"
         :model-value="priority"
-        >Prioritized</BaseCheckbox
-      >
+      >Prioritized</BaseCheckbox>
     </div>
+    <TodoListItemMenu />
   </div>
 </template>
 
 <script>
-import BaseCheckbox from "./BaseCheckbox.vue";
+import BaseCheckbox from "../base/BaseCheckbox.vue";
+import TodoListItemMenu from "./TodoListItemMenu.vue";
+import { computed } from "vue";
+
 export default {
   components: {
     BaseCheckbox,
+    TodoListItemMenu
   },
   props: {
     task: {
       type: Object,
       required: true,
     },
+    projectId: Number,
     done: Boolean,
     priority: Boolean,
+  },
+  provide() {
+    return {
+      task: computed(() => this.task),
+      projectId: computed(() => this.projectId)
+    }
   },
   emits: ["update:done", "update:priority"],
 };
